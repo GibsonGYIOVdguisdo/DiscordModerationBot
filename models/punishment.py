1aa7edd29fe854453e4c6e84df536fb1d098c569
Gibson <Gibson570@hotmail.com>
2025-12-17 19:43:22 +0000

    Defined the models used in MongoDB

---
+++ models/punishment.py
@@ -0,0 +1,11 @@
+from typing import TypedDict
+
+class Punishment(TypedDict):
+    guildId: int
+    memberId: int
+    punisherId: int
+    punishment: str
+    reason: str
+    evidence: str
+    approvers: list[str]
+    date: str
---
+++ models/server.py
@@ -0,0 +1,7 @@
+from typing import TypedDict
+
+class Server(TypedDict):
+    guildId: int
+    roleTrusts: dict[str, int]
+    roleValues: dict[str, int]
+    logChannels: dict[str, int]
