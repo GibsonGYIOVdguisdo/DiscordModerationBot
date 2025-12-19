993e11de8d312475e89dca769a2b0ef7b5b46a7c
Gibson <Gibson570@hotmail.com>
2025-12-19 20:09:19 +0000

    Added types to constructor params

--- context.py
+++ context.py
@@ -1,5 +1,17 @@
+from database.database import Database
+from utils.utils import HelperUtils
+from discord import Client
+from discord.app_commands import CommandTree
+
+
 class BotContext:
-    def __init__(self, database, helper_utils, client, tree):
+    def __init__(
+        self,
+        database: Database,
+        helper_utils: HelperUtils,
+        client: Client,
+        tree: CommandTree,
+    ):
         self.database = database
         self.helper_utils = helper_utils
         self.client = client
